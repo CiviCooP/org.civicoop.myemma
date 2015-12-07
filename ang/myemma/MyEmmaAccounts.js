bc8eab271185365fc9282921da74b9ad43709b88
Jaap Jansma <jaap@edeveloper.nl>
2015-12-07 18:40:35 +0100

    update

--- ang/myemma/MyEmmaAccounts.js
+++ ang/myemma/MyEmmaAccounts.js
@@ -9,12 +9,52 @@
         // under "resolve".
         resolve: {
           accounts: function(crmApi) {
-            return crmApi('MyEmmaAccount', 'get', {});
+            return crmApi('MyEmmaAccount', 'get', {
+              sequential: 1
+            });
+          }
+        }
+      });
+      $routeProvider.when('/my-emma-accounts/:id', {
+        controller: 'MyemmaMyEmmaAccount',
+        templateUrl: '~/myemma/MyEmmaAccountEdit.html',
+
+        // If you need to look up data when opening the page, list it out
+        // under "resolve".
+        resolve: {
+          account: function($route, crmApi) {
+            if ($route.current.params.id !== 'new') {
+              return crmApi('MyEmmaAccount', 'getsingle', {
+                id: $route.current.params.id
+              });
+            } else {
+              return {
+                name: '',
+                public_key: '',
+                private_key: '',
+              };
+            }
           }
         }
       });
     }
   );
+
+  angular.module('myemma').controller('MyemmaMyEmmaAccount', function($scope, crmApi, crmStatus, crmUiHelp, account) {
+    // The ts() and hs() functions help load strings for this module.
+    var ts = $scope.ts = CRM.ts('myemma');
+    $scope.account = account;
+
+    $scope.save = function() {
+      var result = crmApi('MyEmmaAccount', 'create', $scope.account, true);
+      result.then(function(data) {
+        if (data.is_error === 0 || data.is_error == '0') {
+          $scope.account.id = data.id;
+          window.location.href = '#/my-emma-accounts';
+        }
+      });
+    };
+  });
 
   // The controller uses *injection*. This default injects a few things:
   //   $scope -- This is the set of variables shared between JS and HTML.
@@ -26,19 +66,29 @@
     var hs = $scope.hs = crmUiHelp({file: 'CRM/myemma/MyEmmaAccounts'}); // See: templates/CRM/myemma/MyEmmaAccounts.hlp
 
     // We have myContact available in JS. We also want to reference it in HTML.
-    $scope.accounts = accounts.values;
+    $scope.accounts =  accounts.values;
 
-    $scope.save = function save() {
-      return crmStatus(
-        // Status messages. For defaults, just use "{}"
-        //{start: ts('Saving...'), success: ts('Saved')},
-        // The save action. Note that crmApi() returns a promise.
-        /*crmApi('Contact', 'create', {
-          id: myContact.id,
-          first_name: myContact.first_name,
-          last_name: myContact.last_name
-        })*/
-      );
+    $scope.deleteAccount = function deleteAccount(account) {
+      var index = $scope.accounts.indexOf(account);
+      crmApi('MyEmmaAccount', 'delete', {
+        id: account.id
+      }).then(function() {
+        if (index >= 0) {
+          $scope.accounts.splice(index, 1)
+        }
+      });
+    };
+
+    $scope.testconnection = function(account) {
+      crmApi('MyEmmaAccount', 'testconnection', {
+        id: account.id
+      }, {
+        error: function (data) {
+          CRM.alert(data.error_message, ts('Conection failed'), 'error');
+        }
+      }).then(function() {
+        CRM.alert(data.msg, ts('Conection OK'), 'success');
+      });
     };
   });
 
---
+++ api/v3/MyEmmaAccount/Testconnection.php
@@ -0,0 +1,23 @@
+<?php
+
+function civicrm_api3_my_emma_account_Testconnection($params) {
+    if (!isset($params['id'])) {
+        return civicrm_api3_create_error(ts('No ID given'));
+    }
+
+    $account = civicrm_api3('MyEmmaAccount', 'getsingle', array('id' => $params['id']));
+    if (!$account) {
+        return civicrm_api3_create_error(ts('Account not found'));
+    }
+
+    try {
+        $emma = new CRM_Myemma_Emma($account['id'], $account['public_key'], $account['private_key']);
+        $emma->myGroups();
+        return civicrm_api3_create_success(array(
+            'msg' => ts('Successfully connected to my Emma'),
+        ));
+    } catch (Exception $e) {
+        return civicrm_api3_create_error(ts('Could not connect to MyEmma'));
+    }
+
+}
